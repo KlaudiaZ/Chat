bd3bce6e3ec4990cc58893c6a5196b1d3f97de24
KlaudiaZ <klaudia.zajac@email.com>
2018-04-24 17:41:44 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/index.css';
+import './css/other.css';
+import axios from 'axios';
+import $ from 'jquery';
+import navigo from 'navigo';
+console.log('JavaScript was attached to the page!');
+
+$(() => {
+    console.log('content loaded');
+});
